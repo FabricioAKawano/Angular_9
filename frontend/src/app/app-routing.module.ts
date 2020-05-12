ee74abad05645038fa852bb56b4c4e5742717ec4
Fabricio Kawano <fapa_si@yahoo.com.brfapa_si@yahoo.com.br git config --global user.email fapa_si@yahoo.com.br>
2020-05-12 00:39:01 -0300

    estrutura de componentes do produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { HomeComponent } from './home/home.component';     // Add your component here
-// import { AboutComponent } from './about/about.component';  // Add your component here
 
-//This is my case 
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
 const routes: Routes = [
-    // {
-    //     path: '',
-    //     component: HomeComponent
-    // },
-    // {
-    //     path: 'about',
-    //     component: AboutComponent
-    // }
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'products',
+        component: ProductCrudComponent
+    },
+    {
+        path: 'products/create',
+        component: ProductCreateComponent
+    }
 ];
 
 @NgModule({
